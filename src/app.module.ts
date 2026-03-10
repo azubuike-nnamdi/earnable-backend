5ba4415a50f6a7601777a49a23ced1dcf08c15ec
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2026-03-10 21:16:38 +0100

    chore: remove app controller, services and update app module :rocket:

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AppConfigModule } from './config/config.module';
 import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AppConfigModule, DatabaseModule, UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AppConfigModule, DatabaseModule, UsersModule, AuthModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule { }
